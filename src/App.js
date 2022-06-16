6e34f62ca6b61a9ea06a405a470b7a9c84fd40d9
Jamshidbek <jamanu1910077@gmail.com>
2022-06-16 13:58:29 +0500

    uploading shit translation website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react"
+import { useTranslation } from "react-i18next"
+import i18next from "i18next"
+const languages = [
+  {
+    code: "uz",
+    name: "Uzbekcha",
+    country_code: "uzb",
+  },
+  {
+    code: "ru",
+    name: "Русский",
+    country_code: "rus",
+  },
+  {
+    code: "en",
+    name: "English",
+    country_code: "eng",
+  },
+]
 function App() {
+  const { t } = useTranslation()
+
+  const releaseDate = new Date("2021-03-07")
+  const timeDifference = new Date() - releaseDate
+  const number_of_days = Math.floor(timeDifference / (1000 * 60 * 60 * 24))
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="d-flex justify-content-end">
+        <div className="dropdown">
+          <button
+            className="btn btn-default dropdown-toggle"
+            type="button"
+            id="dropdownMenu1"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="true"
+          >
+            Dropdown
+            <span className="caret"></span>
+          </button>
+          <ul className="dropdown-menu" aria-labelledby="dropdownMenu1">
+            {languages.map(({ code, name, country_code }) => (
+              <li key={country_code}>
+                <button
+                  className="dropdown-item"
+                  onClick={() => i18next.changeLanguage(code)}
+                >
+                  <span className={`flag-icon flag-icon-${code} mx-2`}></span>
+                  {name}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <div className="d-flex flex-column align-items-start">
+        <h1 className="font-weight-normal mb-3">{t("welome_message")}</h1>
+        <p>{t("days_since_release", { number_of_days })}</p>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom/client"
+// import App from "./App";
+import i18n from "i18next"
+import { useTranslation, initReactI18next } from "react-i18next"
+import LanguageDetector from "i18next-browser-languagedetector"
+import HttpApi from "i18next-http-backend"
+import "bootstrap/dist/css/bootstrap.min.css"
+import App from "./App"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .use(LanguageDetector)
+  .use(HttpApi)
+  .init({
+    fallbackLng: "en",
+
+    interpolation: {
+      escapeValue: false, // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
+    },
+    detection: {
+      order: ["cookie", "htmlTag", "localStorage", "path", "subdomain"],
+      caches: ["cookie"],
+    },
+
+    backend: {
+      loadPath: "/assets/locales/{{lng}}/translation.json",
+    },
+  })
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
